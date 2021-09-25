6198703a74c8eaabf3e2d4114cc013e40ceb2d84
Dillan Groce <groce.dillan@gmail.com>
2021-09-25 16:48:46 -0400

    end of today's lecture notes

---
+++ 06_Methods/MethodExamples.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _06_Methods
+{
+    [TestClass]
+    public class MethodExamples
+    {
+        [TestMethod]
+            public void MethodExecution()
+        {
+            SayHello("Dillan");
+            int methodReturn = AddTwoNumbers(1, 2);
+            Console.WriteLine(methodReturn);
+        }
+        //--Access Modifer (PUBLIC)
+        //--Return Type (VOID)
+        //--Method Signature (name) (SAYHELLO)
+            //--Parameters (goes inside parenthesis)
+            //--Body - the actual code in the brackets
+        [TestMethod]
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Hello World! And Hello {name}!");
+        }
+        [TestMethod]
+        public int AddTwoNumbers(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+    }
+}
---
+++ 06_Methods/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("06_Methods")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("06_Methods")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77a63128-a8e9-4d90-9f98-7ecc386d924e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
